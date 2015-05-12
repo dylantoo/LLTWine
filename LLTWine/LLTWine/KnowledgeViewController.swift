e29d75bcd5d205239978902b8a0d0cdba869db86
dylantoo <183132459shp@163.com>
2015-05-12 17:00:55 +0800

    创建工程

---
+++ LLTWine/LLTWine/KnowledgeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  KnowledgeViewController.swift
+//  LLTWine
+//
+//  Created by LLToo on 15/5/8.
+//  Copyright (c) 2015年 LLToo. All rights reserved.
+//
+
+import UIKit
+
+class KnowledgeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
